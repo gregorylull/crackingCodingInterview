ae33ab91219c93e53d27465ddd02ecd49be73896
gregorylull <gregorylull@gmail.com>
2014-06-17 21:06:34 -0700

    implementing binary search

---
+++ CCI_ch11_sortingAndSearching.js
@@ -0,0 +1,42 @@
+// ch 11 sorting and searching
+
+/*
+
+Common Sorting Algorithms
+
+  Bubble Sort
+    Runtime: O (n^2) average and worse case. 
+    Memory: O(1)
+
+    swap first two elements if first is greater than second, go to next pair, so on. 
+    iterate through array again
+
+  Selection Sort
+    Runtime: O(n^2) average and worst case.
+    Memory: O(1).
+
+    find the smallest element, and swap with the front element...
+    find the second smallest, and swap with the second element...
+    so on
+
+  Merge Sort
+    Runtime: O(n Log(n)) average and worse case.
+    Memory: Depends
+
+    divides array in half, sort each half, then merge together.
+    same divide/sort/merge logic is applied to each half.
+    MERGE part does heavy lifting
+
+
+Searching Algorithms
+
+  Binary Search 
+    Runtime: O(n Log(n))
+    Memory: Depends
+
+    input array has to be sorted!
+*/
+
+// implement binary search
+
+
---
+++ plan.md
@@ -0,0 +1,36 @@
+How to prepare:
+
+#### Common questions
+Most challenging
+what you learned
+most interesting
+hardest bug
+enjoyed most
+conflicts with teammates
+
+#### Data structures
+linked lists
+binary trees
+tries
+stacks
+queues
+vectors / ArrayLists
+Hash Tables
+
+#### Algorithms
+Breadth first search
+depth first search
+
+binary search -
+merge sort -
+quick sort -
+
+tree insert / find / e.t.c
+
+#### Concepts
+Bit Manipulation
+Singleton Design pattern
+Factory design Pattern
+Memory (Stack vs Heap)
+Recursion
+Big-O Time
