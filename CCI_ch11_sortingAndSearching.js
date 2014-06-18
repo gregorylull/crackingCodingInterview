15a35a736ba4b663a4c9c75253344ed06a62528e
gregorylull <gregorylull@gmail.com>
2014-06-18 12:18:30 -0700

    implemented binarySearch, accounts for values outside of sorted array range

--- CCI_ch11_sortingAndSearching.js
+++ CCI_ch11_sortingAndSearching.js
@@ -37,6 +37,126 @@ Searching Algorithms
     input array has to be sorted!
 */
 
-// implement binary search
+/* HELPER FUNCTIONS
 
+*/
+
+// create a range function, ends are inclusive
+function createRange (min, max, step, random) {
+	min = min || 0;
+	max = max || 10;
+	step = step || 1;
+	random = random || false;
+
+	var result = [];
+	for (var i = min; i <= max; i += step) {
+		result.push(i);
+	}
+
+	// randomize
+	if (random) {
+		for (var j = 0; j < result.length; j++) {
+			var randIndex = Math.floor((Math.random() * result.length));
+			swapElements(result, j, randIndex);
+		}
+	}
+
+	return result;
+}
+
+function swapElements(array, indexA, indexB) {
+	var temp = array[indexA];
+	array[indexA] = array[indexB];
+	array[indexB] = temp;
+
+	return array;
+}
+
+var executeHelper = function () {
+	var range0 = createRange(2, 10, 2);
+	var r00 = range0[0] === 2;
+	var r01 = range0[4] === 10;
+	var r02 = range0.length === 5;
+
+	var range1 = createRange(0, 9);
+	var r03 = range1.length === 10;
+	var r04 = range1[0] === 0;
+	var r05 = range1[9] === 9;
+
+	var range2 = createRange(0, 9, 1, true);
+
+	console.log(r00, r01, r02, r03, r04, r05);
+	console.log(range2);
+};
+
+// executeHelper();
+
+// binary search
+/*
+  - input has to be sorted (merge sort)
+  - find midIndex
+  - check midIndex
+  - if smaller -> run bin search on smaller portion of array
+  - if larger -> run bin search on larger partition of array
+  * using slice would negate the benefits of binary search
+  * need to keep track of beginning and endIndex of partition // inclusive
+  * two pointers
+
+  [0]                 // mid: 
+  [0, 1]              // 
+  [0, 1, 2]
+  [0, 1, 2, 3]
+  [0, 1, 2, 3, 4]
+  [0, 1, 2, 3, 4, 5]
+*/
+// returns true if it contains, false if not. Inclusive
+var binarySearch = function (array, target, beginIndex, endIndex, cycles) {
+	// default corner case check
+	if (array.length === 0 || target < array[0] || target > array[array.length-1]) { return false; }
+
+	// default values
+	beginIndex = beginIndex || 0;
+	endIndex = endIndex || array.length-1; // instead of length, we calculate based on index position
+	cycles = cycles || 0;
+
+  // addition and math.floor gets the midpoint
+	var midIndex = Math.floor((endIndex + beginIndex)/2); 
+
+
+	// base case, if we're on the same index
+	// terminate if we found the target
+	if (array[midIndex] === target) {
+		console.log('found: ', target, true, '  cycles: ', cycles);
+		return true;
+
+	// if we DIDN'T find the target, AND the beginning and end point to the same location
+	// then we have exhausted our search options
+	} else if (beginIndex === endIndex) {
+		console.log('found: ', target, false, ' cycles: ', cycles);
+		return false;
+
+  // if target is larger, use the larger portion
+	} else if (target > array[midIndex]) {
+		return binarySearch(array, target, midIndex+1, endIndex, cycles+1);
+
+	// if target is smaller, use the smaller portion
+	} else {
+		return binarySearch(array, target, beginIndex, midIndex-1, cycles+1);
+	}
+
+};
+
+executeBinarySearch = function () {
+	var range0 = createRange(0, 10);
+	for (var i = -2; i <= 12; i++) {
+		binarySearch.apply(null, [range0, i]);
+	}
+
+  // var range1 = createRange(0,100);
+  // var r01 = binarySearch(range1, 99);
+
+
+};
+
+executeBinarySearch();
 
